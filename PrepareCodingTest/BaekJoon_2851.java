caf86938662b7fd1121f2b5ab42404be90619bd9
Song-Hyun-Jung <57795722+Song-Hyun-Jung@users.noreply.github.com>
2022-08-16 14:11:02 +0900

    부분합

--- PrepareCodingTest/BaekJoon_2851.java
+++ PrepareCodingTest/BaekJoon_2851.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.lang.Math;
+//부분합
 public class Main{
+    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
